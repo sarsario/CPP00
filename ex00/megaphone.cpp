be5eb6485f2433c16dfb70bed0b92bc422fcf1f6
osarsari <o.sarsari@gmail.com>
2023-10-23 11:20:49 +0200

    megaphone

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/23 10:58:34 by osarsari          #+#    #+#             */
+/*   Updated: 2023/10/23 10:59:23 by osarsari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		i = 1;
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				std::cout << (char)toupper(argv[i][j]);
+				j++;
+			}
+			i++;
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
